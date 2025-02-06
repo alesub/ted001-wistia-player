bcdf5739e6df20e1282cd2f3932a48aa1ff84a7b
Alejandro Medina <alejandro@radishlab.com>
2025-02-06 12:56:30 -0300

    Add wistia elements to custom d ts

---
+++ custom.d.ts
@@ -0,0 +1,6 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        'wistia-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'wistia-transcript': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+}
