714237076a0de498757c3a8ef33104a8b93b7dd1
Alejandro Medina <alejandro@radishlab.com>
2025-02-06 18:27:48 -0300

    Move custom element declaration

--- custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace JSX {
-    interface IntrinsicElements {
-        'wistia-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        'wistia-transcript': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-}
---
+++ src/types/custom.d.ts
@@ -0,0 +1,12 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'wistia-player': {
+      'media-id': string;
+      'class': string;
+    };
+    'wistia-transcript': {
+      'media-id': string;
+      'class': string;
+    };
+  }
+}
